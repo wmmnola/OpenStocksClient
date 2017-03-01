fbaaa5d5c2c15b3f23e5ba7a873405db8d56ae45
wmmnola <wmmnola@gmail.com>
2017-03-01 14:10:06 -0600

    Started work on buy functionality

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 var socket;
 var id;
+var companies;
 $(document).ready(main);
 //main();
 
@@ -54,12 +55,30 @@ function showNameRequest(data) {
 }
 
 function mainGamePhase(data) {
-  var companies = data.companies;
+  companies = data.companies;
   $("#namecompany").hide();
   for (var i = 0; i < companies.length; i++) {
     $('#market').append('<tr><td>' + companies[i].identifer + '</td><td>' +
       companies[i].value + '</td><td>' + companies[i].sharevalue +
+      '</td><td><button id=' + companies[i].identifer + '>buy</button>' +
       '</td></tr>');
+    $('#' + companies[i].identifer).click(buyStockHandler);
+    console.log(companies[i].identifer + " added");
   }
   $("#game").show();
+}
+
+function buyStockHandler(event) {
+  console.log("working?");
+  event.stopPropagation();
+  console.log(event.target.id);
+  var toBuy = find_by_identifer(event.target.id);
+  console.log(toBuy);
+}
+
+function find_by_identifer(iden) {
+  for (var i = 0; i < companies.length; i++) {
+    if (companies[i].identifer == iden) return companies[i];
+  }
+  throw "Fatal Error, identifer is not valid";
 }
