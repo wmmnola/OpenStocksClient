80e55d14a96340d9aa78600e19a998c669afc408
wmmnola <wmmnola@gmail.com>
2017-03-01 18:45:26 -0600

    Restructured filetype, everything is split between 3 js files, that way I dont end up with a single 1000 line javascript file

--- client.js
+++ client.js
@@ -3,7 +3,6 @@ var id;
 var companies;
 var orders = [];
 $(document).ready(main);
-//main();
 
 function main() {
   $("#namecompany").hide();
@@ -27,23 +26,6 @@ function main() {
   socket.on("NameRequest", showNameRequest);
   socket.on("sendData", mainGamePhase);
   $(".endTurn").click(endTurnHandler);
-
-}
-
-//$("#nameCompany").submit(nameMyCompany);
-
-function nameMyCompany(event) {
-  //console.log(id);
-  event.stopPropagation();
-  var company = $("#name").val();
-  var iden = $("#identifier").val()
-  var data = {
-    id: id,
-    name: company,
-    identity: iden
-  };
-  console.log(data);
-  socket.emit("NameReply", data);
 
 }
 
@@ -81,47 +63,4 @@ function mainGamePhase(data) {
     if (button != "") $('#' + companies[i].identifer).click(buyStockHandler);
   }
   $("#game").show();
-}
-
-function buyStockHandler(event) {
-
-  console.log("working?");
-  event.stopPropagation();
-  console.log(event.target.id);
-  var toBuy = find_by_identifer(event.target.id);
-  $("." + toBuy.identifer + "buy").remove();
-  console.log(toBuy);
-  var order = find_order(toBuy)
-  if (order) {
-    order.buy();
-  } else {
-    order = new Order(toBuy);
-    order.buy();
-    orders.push(order);
-  }
-  $('.' + toBuy.identifer).append("<td class=" + toBuy.identifer + "buy>" +
-    order.buyAmount + " </td>");
-}
-
-function find_by_identifer(iden) {
-  for (var i = 0; i < companies.length; i++) {
-    if (companies[i].identifer == iden) return companies[i];
-  }
-  throw "Fatal Error, identifer is not valid";
-}
-
-function find_order(company) {
-  for (var i = 0; i < orders.length; i++) {
-    if (orders[i].company == company) return orders[i];
-  }
-  return false;
-}
-
-function endTurnHandler(event) {
-  event.stopPropagation();
-  var payload = {
-    id: id,
-    orders: []
-  };
-  socket.emit("endTurn", payload);
 }
